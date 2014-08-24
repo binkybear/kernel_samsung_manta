1b8039a04475fc68ee3c7ff080847c260321b0cb
Craig Gomez <craigacgomez@gmail.com>
2014-08-24 12:23:07 -0700

    Add ROW and SIO I/O schedulers. Make ROW the default I/O scheduler.

--- setenv.sh
+++ setenv.sh
@@ -4,3 +4,4 @@ export PATH=$PATH:../THUNDERKAT/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
